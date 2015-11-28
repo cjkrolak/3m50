b6d1f5eeeb138bc4b2fc0bf57493c7b1b7255933
Christopher Krolak <christopher.j.krolak@seagate.com>
2015-11-27 19:07:56 -0600

    added interactive mode and additional comments and print text

--- 3m50.py
+++ 3m50.py
@@ -1,8 +1,7 @@
-import radiotherm
-
 import TStat
 import API
 
+import sys
 import time
 
 taddr = '192.168.1.106'  # home
@@ -43,6 +42,7 @@ def Connect_Tstat(taddr):
     # tstat = TStat.TStat(taddr, api=API.API_CT30v192(), logger=None)
     tstat = TStat.TStat(taddr, logger=None)
 
+    print "thermostat version info:"
     print "current model...%s" % tstat.getModel()
     print "current version...%s" % tstat.getVersion()
     print "current mode...%s" % tstat.getMode()
@@ -50,7 +50,7 @@ def Connect_Tstat(taddr):
     print "current wifi security...%s" % tstat.getSecurity()
     # print "current night light status...%s" % tstat.getNightLight()
     print "\n"
-
+    print "current settings:"
     print "current temp...%s" % tstat.getCurrentTemp()
     print "current setpoint...%s" % tstat.getHeatPoint()
     print "current holdstate...%s" % tstat.getHoldState()
@@ -70,9 +70,48 @@ def Connect_Tstat(taddr):
     return tstat
 
 
+def interactive_TStat(TStat):
+    ''' function provides interactive dialog
+    '''
+    # methods supported
+    # keys = ['set', 'get', 'esc']
+
+    key_dict = []
+    while True:
+        key_input = raw_input("\n\ninteractive mode, enter method and parameter ('esc' to exit): ")
+        key_input.strip('\t\n\r')
+        if ' ' in key_input:
+            key_dict = key_input.split(' ')
+        else:
+            key_dict.append(key_input)
+        if len(key_dict) >= 1:
+            method = key_dict[0]
+        if len(key_dict) >= 2:
+            param = key_dict[1]
+        if len(key_dict) >= 3:
+            valu = key_dict[2]
+
+        if method == 'get':
+            if len(key_dict) != 2:
+                print "\nparse error in string '%s', 'get' method requires 1 parameter, exiting..." % key_input
+                sys.exit(-1)
+            print "\nparam '%s' returns: %s" % (param, TStat._get(param))
+        if method == 'set':
+            if len(key_dict) != 3:
+                print "\nparse error in string '%s', 'set' method requires 2 parameters, exiting..." % key_input
+                sys.exit(-1)
+            print "\nparam '%s' was set to: %s, changing to: %s" % (param, TStat._get(param), valu)
+            TStat._set(param, valu)
+            print "\nparam '%s' returns: %s" % (param, TStat._get(param))
+        elif method == 'esc':
+            print "\nescape sequence detected, exiting interactive mode..."
+            sys.exit(0)
+
+
 if __name__ == "__main__":
     # tstat1 = Connect_radiotherm(taddr)
 
     tstat1 = Connect_Tstat(taddr)
+    interactive_TStat(tstat1)
 
     # Reboot(tstat1)
